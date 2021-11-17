269513a3acdac907b0d9d8cdf3373cd60b5e857f
Yuliya <jknoroz@gmail.com>
2021-11-18 00:21:03 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,93 +1,28 @@
 import React, { Component } from 'react';
 
-import ContactForm from './components/ContactForm/ContactForm';
-import ContactList from './components/ContactList/ContactList';
-import Filter from './components/Filter/Filter';
-
 import './App.css';
-import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  formSubmitHandler = data => {
-    console.log(data);
-  };
-
-  addContact = ({ name, number }) => {
-    const contact = {
-      id: shortid.generate(),
-      name,
-      number,
-    };
-
-    const contactIsInList = this.state.contacts.find(
-      cont => cont.name.toLowerCase() === contact.name.toLowerCase(),
-    );
-
-    if (contactIsInList) {
-      alert(`${contact.name} is already in contacts!`);
-    } else {
-      this.setState(({ contacts }) => ({
-        contacts: [contact, ...contacts],
-      }));
-    }
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+    images: null,
+    loading: false,
   };
 
   componentDidMount() {
-    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
+    this.setState({ loading: true });
+    fetch(
+      'https://pixabay.com/api/?q=cat&page=1&key=22088587-b9222ac51e20698a54a4430fc&image_type=photo&orientation=horizontal&per_page=12',
+    )
+      .then(res => res.json())
+      .then(images => this.setState({ images }))
+      .finally(() => this.setState({ loading: false }));
   }
 
   render() {
-    const visibleContacts = this.getVisibleContacts();
     return (
       <div className="App">
-        <div>
-          <h1>Phonebook</h1>
-          <ContactForm onSubmit={this.addContact} />
-
-          <h2>Contacts</h2>
-          <Filter value={this.state.filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </div>
+        {this.state.loading && <h1>Loading...</h1>}
+        {this.state.images && <div>Pokemon</div>}
       </div>
     );
   }
