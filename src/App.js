3f539b6c9347fe5e96206eb90d03ffd693a484d8
Yuliya <jknoroz@gmail.com>
2021-11-19 00:37:45 +0200

    form

--- src/App.js
+++ src/App.js
@@ -5,24 +5,28 @@ import SearchBar from './components/Searchbar/Searchbar';
 
 class App extends Component {
   state = {
-    images: null,
+    searchRequest: '',
     loading: false,
   };
 
-  componentDidMount() {
-    this.setState({ loading: true });
-    fetch(
-      'https://pixabay.com/api/?q=cat&page=1&key=22088587-b9222ac51e20698a54a4430fc&image_type=photo&orientation=horizontal&per_page=12',
-    )
-      .then(res => res.json())
-      .then(images => this.setState({ images }))
-      .finally(() => this.setState({ loading: false }));
-  }
+  // componentDidMount() {
+  //   this.setState({ loading: true });
+  //   fetch(
+  //     'https://pixabay.com/api/?q=cat&page=1&key=22088587-b9222ac51e20698a54a4430fc&image_type=photo&orientation=horizontal&per_page=12',
+  //   )
+  //     .then(res => res.json())
+  //     .then(images => this.setState({ images }))
+  //     .finally(() => this.setState({ loading: false }));
+  // }
+
+  handleFormSubmit = searchRequest => {
+    this.setState({ searchRequest });
+  };
 
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar onSubmit={this.handleFormSubmit} />
         {/* {this.state.loading && <h1>Loading...</h1>}
         {this.state.images && <div>Pokemon</div>} */}
       </div>
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,14 +1,30 @@
 import { Component } from 'react';
 import s from './Searchbar.module.css';
 import { BiSearch } from 'react-icons/bi';
+
 export default class SearchBar extends Component {
   state = {
-    images: '',
+    searchRequest: '',
+  };
+
+  handleSearch = e => {
+    this.setState({ searchRequest: e.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.searchRequest.trim() === '') {
+      alert('Put in image name');
+      return;
+    }
+
+    this.props.onSubmit(this.state.searchRequest);
+    this.setState({ searchRequest: '' });
   };
 
   render() {
     return (
-      <header className={s.Searchbar}>
+      <header className={s.Searchbar} onSubmit={this.handleSubmit}>
         <form className={s.SearchForm}>
           <button type="submit" className={s.SearchFormButton}>
             <span className={s.SearchFormButtonLabel}>
@@ -22,6 +38,8 @@ export default class SearchBar extends Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            value={this.state.searchRequest}
+            onChange={this.handleSearch}
           />
         </form>
       </header>
